eae4f4ce899aebbb9b3bcd22921ed4a203327b94
dufok <duffokin@gmail.com>
2023-01-06 10:21:41 -0300

    change files

--- send_to_s3/index.py
+++ send_to_s3/index.py
@@ -48,5 +48,5 @@ def upload():
     return render_template("file_upload_to_s3.html",msg =msg)
 
 
-if __name__ == "__main__":app.run(host='0.0.0.0', port=80)
-    #app.run(debug=True)
+if __name__ == "__main__":app.run(debug=True)
+  #app.run(host='0.0.0.0', port=80)
