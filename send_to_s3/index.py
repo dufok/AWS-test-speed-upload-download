9f5f73ff453a0051a5b3dda3f2630dea3874c288
dufok <duffokin@gmail.com>
2023-01-07 01:51:52 -0300

    rm region of S3

--- send_to_s3/index.py
+++ send_to_s3/index.py
@@ -11,7 +11,8 @@ from werkzeug.utils import secure_filename
 s3 = boto3.client('s3',
                     aws_access_key_id=os.environ['S3_ACCESS_KEY'],
                     aws_secret_access_key=os.environ['S3_SECRET_KEY'],
-                    endpoint_url=os.environ['S3_END_POINT']
+                    #endpoint_url=os.environ['S3_END_POINT']
+                  #process.env.S3_REGION || 'eu-west-1'
                      )
 
 BUCKET_NAME='speedtestingbucketonlydeletit'
