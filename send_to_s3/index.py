b396fc95835394bceff38a999104c9dbca92088d
dufok <duffokin@gmail.com>
2023-01-07 01:40:55 -0300

    add in one run dose storej

--- send_to_s3/index.py
+++ send_to_s3/index.py
@@ -45,14 +45,52 @@ def upload():
                 
                 # Stop the timer and calculate the elapsed time
                 end_time = time.time()
-                elapsed_time = end_time - start_time
+                elapsed_time_aws = end_time - start_time
                 
-                # Display the elapsed time
-                print("Elapsed time:", elapsed_time)
+
+s3 = boto3.client('s3',
+                    aws_access_key_id=os.environ['STORJ_ACCESS_KEY'],
+                    aws_secret_access_key=os.environ['STORJ_SECRET_KEY'],
+                    endpoint_url=os.environ['STORJ_END_POINT']
+                     )
+
+BUCKET_NAME='speedtestingbucketonlydeletit'
+
+@app.route('/upload',methods=['post'])
+def upload():
+    if request.method == 'POST':
+        img = request.files['file']
+        if img:
+                # Start the timer
+                start_time = time.time()
+            
+                # Perform the file upload
+                filename = secure_filename(img.filename)
+                # Save the file to a temporary location
+                with tempfile.TemporaryDirectory() as temp_dir:
+                    temp_path = os.path.join(temp_dir, filename)
+                    img.save(temp_path)
+                    # Open the file in binary mode
+                    with open(temp_path, 'rb') as file:
+                        # Use the upload_fileobj method to safely upload the file
+                        s3.upload_fileobj(
+                            Fileobj=file,
+                            Bucket=BUCKET_NAME,
+                            Key=filename
+                        )
+                
+                # Stop the timer and calculate the elapsed time
+                end_time = time.time()
+                elapsed_time_storj = end_time - start_time
+                
+                
+                
+                
                 
                 msg = "Upload Done !"
-                time_msg = ("Elapsed time (sec):", elapsed_time)
-    return render_template("file_upload_to_s3.html",msg =msg, time_msg =time_msg)
+                time_msg_aws = elapsed_time_aws
+                time_msg_storj = elapsed_time_storj
+    return render_template("file_upload_to_s3.html",msg =msg, time_msg_aws =time_msg_aws, time_msg_storj =time_msg_storj)
 
 
 
