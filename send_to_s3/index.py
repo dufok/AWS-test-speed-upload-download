60968d43595ca619ef09e0c12822ee3743bfd962
dufok <duffokin@gmail.com>
2023-01-06 23:55:37 -0300

    correct import part

--- send_to_s3/index.py
+++ send_to_s3/index.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, render_template, request
 import boto3
-from botocore.exeptions import NoCredentialsError
+from botocore.exceptions import NoCredentialsError
 import requests
 import mimetypes
 import time
